b477654b484c70ebd49b9cfa4b20b6945e47b2e4
canoe <canoe_likecode@163.com>
2025-04-04 20:11:58 +0800

    优化项目列表组件的代码结构，调整标签和链接的样式，更新项目描述和链接，确保页面内容更清晰易读。

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -12,54 +12,52 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    name: 'Devaradise.com',
-    description: 'A blog that sharing web development resources and tutorials',
-    demoLink: 'https://devaradise.com',
-    tags: ['Blog']
-  },
-  {
-    name: 'Sellercraft App',
-    description: 'An Ecommerce omnichannel platform in Southeast Asia',
-    demoLink: 'https://sellercraft.co',
+    name: 'FocusFour',
+    description: '一款基于四象限任务管理法的提醒事项增强工具，帮你把握重要的事',
+    demoLink: 'https://apps.apple.com/us/app/focusfour-task-daily-planner/id6661031104',
     demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['ECommerce', 'Saas']
+    tags: ['Productivity', 'App']
   },
   {
-    name: 'Gaji.id App',
-    description: 'Payroll and HR Management Information System',
-    demoLink: 'https://sellercraft.co',
+    name: 'Phorase',
+    description: '一款如魔术般的 AI 修图工具，轻松消除照片中的杂物与干扰',
+    demoLink: 'https://apps.apple.com/sg/app/phorase/id6497569235',
     demoLinkRel: 'nofollow noopener noreferrer',
-    tags: ['HRIS', 'Saas']
+    tags: ['AI', 'Photo', 'App']
   },
   {
-    ...(await getRepositoryDetails('devaradise/paradise-ui')),
-    name: 'Paradise UI',
-    demoLink: 'https://paradise-ui.com',
-    postLink: 'https://devaradise.com/how-i-build-paradise-ui-react-component-library/',
-    tags: ['React', 'UI Library']
+    name: 'IndieTO',
+    description: '由我精心筛选的独立开发全流程实用工具集，帮助你快速启动和发布产品',
+    demoLink: 'https://indieto.com/',
+    demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ['Tools', 'IndieHacker']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/react-lab')),
-    name: 'React Lab',
-    demoLink: 'https://devaradise.com/lab/react/',
-    tags: ['React']
+    name: 'LinkToMarkdown',
+    description: '一个简单且现代化的免费工具，从链接中提取文档并生成 Markdown',
+    demoLink: 'https://linktomarkdown.com/',
+    demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ['Tools', 'Markdown']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/pokemon-catcher')),
-    name: 'Pokemon Catcher',
-    demoLink: 'https://pokemon-catcher-18636.web.app/',
-    tags: ['Hobby']
+    name: 'LET\'S VISION 2025',
+    description: '国内首个空间计算+AI融合的顶级技术盛会，参与组织以及负责网站搭建',
+    demoLink: 'https://letsvision.swiftgg.team/',
+    demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ['Event', 'Spatial Computing', 'AI']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/movie-nominations')),
-    name: 'Movie Nominations',
-    demoLink: 'https://movie-nominations-c21c3.web.app/',
-    tags: ['Hobby']
+    name: 'Let\'s visionOS 2024',
+    description: '一场属于全世界的visionOS创造者大会，参与组织和以及负责网站搭建',
+    demoLink: 'https://letsvisionos24.swiftgg.team/',
+    demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ['Event', 'visionOS']
   },
   {
-    ...(await getRepositoryDetails('syakirurahman/organization-tree')),
-    name: 'Organization tree',
-    demoLink: 'https://organization-tree-2a446.web.app/',
-    tags: ['Hobby']
+    name: 'Twine by SwiftGG',
+    description: '参与开发的一款帮助开发者更轻松地学习苹果官方文档的浏览器插件',
+    demoLink: 'https://github.com/SwiftGGTeam/swiftgg-trans-plugin',
+    demoLinkRel: 'nofollow noopener noreferrer',
+    tags: ['Plugin', 'iOS', 'SwiftGG']
   }
 ]
