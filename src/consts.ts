ccb4da09994123212c5e43ead51e0232e2ed438c
canoe <canoe_likecode@163.com>
2025-04-07 09:51:21 +0800

    update

--- src/consts.ts
+++ src/consts.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = '轻舟';
-export const SITE_TAGLINE = '一边感受，一边创造';
+export const SITE_TAGLINE = '一边感知，一边创造';
 export const SITE_DESCRIPTION = '记录独立开发、产品设计、AI 工具和数字生活的个人博客。'; // 用于 meta description，利于 SEO
